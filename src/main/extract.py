48918129a97be14047a74a84dd368d72cfc075c6
malik-shaik-kr <malik.shaik@kroger.com>
2024-09-26 16:15:33 +0530

    Params

--- src/main/extract.py
+++ src/main/extract.py
@@ -1,3 +1,8 @@
 # Databricks notebook source
+from pprint import pformat
+
+print("Fetching Params...")
+params: dict = dbutils.widgets.getAll()
+print(pformat(params))
 
 print("The data has been successfully extracted!")
