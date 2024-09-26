710fbfc950e6297b3e2bcdc5edac9703a9df96ba
malik-shaik-kr <malik.shaik@kroger.com>
2024-09-26 14:29:21 +0530

    Python File added

---
+++ src/main/Notebook.py
@@ -0,0 +1,12 @@
+# Databricks notebook source
+from pprint import pformat
+
+# COMMAND ----------
+
+dbutils.widgets.text(name="BDS_DB_NAME", defaultValue="release_test")
+BDS_DB_NAME = dbutils.widgets.get(name="BDS_DB_NAME")
+
+# COMMAND ----------
+
+params: dict = dbutils.widgets.getAll()
+print(pformat(params))
