52ab7b0d35479c56ccbf2019d2defa8ab578d80c
malik-shaik-kr <malik.shaik@kroger.com>
2024-11-07 18:46:23 +0530

    Datadog Integration - 1

--- src/main/Notebook.py
+++ src/main/Notebook.py
@@ -1,14 +1,62 @@
 # Databricks notebook source
+import logging
+import random
+import time
+from datetime import datetime, timezone
 from pprint import pformat
 
-# COMMAND ----------
-
-dbutils.widgets.text(name="BDS_DB_NAME", defaultValue="release_test")
-BDS_DB_NAME = dbutils.widgets.get(name="BDS_DB_NAME")
+import pytz
+from diem.monitoring.tracker import Application, MedallionLayer, Tracker
 
 # COMMAND ----------
 
-params: dict = dbutils.widgets.getAll()
+start_dt = datetime.now(timezone.utc)
+
+# COMMAND ----------
+
+params: dict = {}
+try:
+    params = dbutils.widgets.getAll()
+except Exception as e:
+    print("Failed to parse Params")
 print(pformat(params))
 
-print("SUCCESS: 1")
+# COMMAND ----------
+
+bds_dummy_count: int = random.randint(1, 999999)
+
+tracker = Tracker(
+    Application(
+        domain="BDS",
+        name="BDS-QA-AUTOMATION",
+        job_id=params.get("JOB_ID", 1),
+        run_id=params.get("RUN_ID", 1),
+        transaction_id=0,
+        tags={"env": "Development", "searchKey": "BDS_QA_DD"},
+    )
+)
+tracker.logger.setLevel(logging.INFO)  ## Set Log Level
+
+# COMMAND ----------
+
+end_dt = datetime.now(timezone.utc)
+
+# COMMAND ----------
+
+# Now use bds_count in the tracker.record function
+tracker.record(
+    layer=MedallionLayer.LANDING,
+    records=bds_dummy_count,
+    accepted=bds_dummy_count,
+    violations=1,
+    duration=(end_dt - start_dt).total_seconds(),
+    log={
+        "name": "bds-dummy-daily-count",
+        "transaction_date": datetime.now(pytz.utc).isoformat(),
+        "bds_dummy_count": bds_dummy_count,
+        **params,
+    },
+)
+
+###### Query to search in datadog:
+#####  index:datadog-bulkdataservices-index env:nonprod -source:(spark OR stdout) @application:BDS-QA-AUTOMATION
